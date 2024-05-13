421550e92d29210c24bd6f47d07ea08c1ebd6cd4
Stina Persson <stpe1901@student.miun.se>
2024-05-13 10:43:43 +0200

    skal för webbtjänst skapad

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors()); 
 
+//Routing FULL CURD
+app.get("/api", (req,res)=>{
+    res.json({message:"Välkommen till mitt API"})
+}); 
+
+app.get("/api/menu", (req, res) => {
+    res.json({message: "Menyn som finns sparad skrivs ut"})
+}); 
+
+app.post("/api/menu", (req, res)=> {
+    res.json({message: "menyn uppdateras(ska vara skyddad!)"}); 
+})
+
+app.put("/api/menu", (req, res)=> {
+    res.json({message: "Uppdaterar menyn(ska vara skyddad!)"}); 
+})
+
+app.delete("/api/menu", (req, res) => {
+    res.json({message: "Tar bort sak från menyn(ska vara skyddad!)"});
+}); 
 
 //Startar servern på angiven port
 app.listen(port, ()=>{

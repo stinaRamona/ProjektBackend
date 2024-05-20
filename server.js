e54766f6c284e4ece4486a70ffb7d2d396ff1eec
Stina Persson <stpe1901@student.miun.se>
2024-05-20 15:39:01 +0200

    Kan lägga till rätter till menyn som läggs till i atlas

--- server.js
+++ server.js
@@ -19,23 +19,65 @@ mongoose.connect(process.env.DATABASE).then(() => {
     console.error("Något gick fel med anslutningen"); 
 }); 
 
+//skapa scehma för meny
+const menuSchema = new mongoose.Schema({
+    dishName: {
+        type: String, 
+        required: true
+    }, 
+    price: {
+        type: Number, 
+        required: true
+    }, 
+    description: {
+        type: String, 
+        required: true
+    }, 
+    allergens: {
+        type: String, 
+        required: false
+    }
+}); 
+
+//skapar en model baserat på meny schemat
+const MenuItem = mongoose.model("MenuItem", menuSchema); 
+
 //Routing FULL CURD
 app.get("/api", (req,res)=>{
-    res.json({message:"Välkommen till mitt API"})
+    res.json({message:"Välkommen till degknytets API"})
 }); 
 
+//hämta in menylaternativ
 app.get("/api/menu", (req, res) => {
-    res.json({message: "Menyn som finns sparad skrivs ut"})
+    try{
+        result= MenuItem.find({}) // Visar alla menyalternativ
+
+        return res.json(result); 
+
+    } catch(error) {
+        return res.status(500).json(error); 
+    }
 }); 
 
+//lägg till nya menyalternativ
 app.post("/api/menu", (req, res)=> {
-    res.json({message: "menyn uppdateras(ska vara skyddad!)"}); 
-})
+    try {
+        let result = MenuItem.create(req.body); 
 
+        return res.json("Ny rätt tillagd" + result)
+
+    } catch(error){
+
+        return res.json(error); 
+    }
+}); 
+
+//uppdatera menyalternativ
 app.put("/api/menu", (req, res)=> {
     res.json({message: "Uppdaterar menyn(ska vara skyddad!)"}); 
 })
 
+//ta bort menyalternativ
 app.delete("/api/menu", (req, res) => {
     res.json({message: "Tar bort sak från menyn(ska vara skyddad!)"});
 }); 

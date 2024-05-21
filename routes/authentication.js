63162e561418f7c95ab7b9b7af04c8336b47be7a
Stina Persson <stpe1901@student.miun.se>
2024-05-21 10:53:12 +0200

    Börjat med auth koll

---
+++ routes/authentication.js
@@ -0,0 +1,29 @@
+//requires
+const express = require("express"); 
+const router = express.Router(); 
+const jwt = require("jsonwebtoken"); 
+const bcrypt = require("bcrypt"); 
+const mongoose = require("mongoose"); 
+
+require("dotenv").config(); 
+
+//Anslut till databas
+mongoose.set("strictQuery", false); 
+mongoose.connect(process.env.DATABASE); 
+
+//Route för login för att komma åt routen för att uppdatera, lägga till och ta bort menyalternativ
+router.post("/login", async (req, res)=> {
+    try {
+        const {username, password} = req.body; 
+
+        if(!username || !password) {
+            res.status(401).json({message: "Både användarnamn och lösenord måste anges"}); 
+        }
+        
+    } catch {
+
+    }
+}); 
+
+//exportera
+module.exports = router; 
